134c209fc530de51c2e68e234b8313c04b77cf27
Alex Segundo <asegundo442@gmail.com>
2020-10-03 22:33:05 -0500

    init project

---
+++ README.md
@@ -0,0 +1,6 @@
+
+## PAGINA WEB DEL CENTRO POBLADO DE JANQUE
+> La pagina web del [centro poblado de Janque](http://localhost:8000/) esta hecha precisamente para mostrar la belleza de su pueblo, y hacerla publica en el mundo.
+>> Sus costumbres y todo.
+## PROGRAMADO POR [Alex Segundo](https://github.com/Segundo-hub)
+> Linkedin [Alex Segundo](https://www.linkedin.com/in/alex-segundo/?target=_blank)
---
+++ Server/server.js
@@ -0,0 +1,38 @@
+
+//Libraries
+const express = require("express");
+const path = require("path");
+
+//EXPRESS APP
+const app = express();
+
+app.set('port', process.env.PORT || 8080)
+
+//Absolute Paths
+const pathResolve = path.resolve(`${__dirname}/../Public`);
+console.log(pathResolve)
+
+app.use(express.static(path.join(pathResolve)));
+
+//GETTERS
+app.get("/", (input, print) => {
+ 	print.sendFile(path.join(__dirname + '/../Public/index.html'));	
+	console.log('runing in port', app.get('port'));
+})
+
+app.get("/history", (input, print) => {
+	print.status(200).sendFile(path.join(__dirname + '/../Public/history.html'));
+	console.log('send history file im port', app.get('port'))
+})
+
+app.get("*", (input, print) => {
+	print.status(404).sendFile(path.join(__dirname + '/../Public/404.html'))
+	console.log("error", 404)
+})
+
+
+
+//Server Port
+app.listen(app.get('port'), ()=> {
+	console.log(`Your app listen in port ${app.get('port')}`);
+});
